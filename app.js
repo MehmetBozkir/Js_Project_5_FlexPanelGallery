3cd23fa954dac93b880cbe69b8f6fb348538cfce
MehmetBozkir <mehmetsalihbozkir@gmail.com>
2023-12-18 16:41:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function toggleOpen() {
+  console.log("HelloMehmet");
+  this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
